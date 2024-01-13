c37e7a5e36e5b781862f54e4da79f9c4bfc492e1
Miali1865 <mialivolarandrianaivo@gmail.com>
2024-01-13 11:04:53 +0100

    Mialivola modif

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { EventEmitter } from 'events';
+EventEmitter.defaultMaxListeners = 15; // Vous pouvez ajuster le nombre selon vos besoins
 
 
 const container = document.getElementById('root');
